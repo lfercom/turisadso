d1f1e2d4b5fe1a9c25ef7b2db0b6b72447cd2a8d
LuisFernandoCorredor <lfercom@gmail.com>
2025-06-23 14:15:23 -0500

    Creacion del proyecto

---
+++ turisadso/android/app/src/main/kotlin/com/example/turisadso/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.turisadso
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ turisadso/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Principal());
+}
+
+class Principal extends StatelessWidget {
+  const Principal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Column(
+          children: [
+            Text("Hola Mundo"),
+            Text("Servicio Nacional de Aprendizaje - SENA"),
+          ],
+        ),
+        debugShowCheckedModeBanner: false);
+  }
+}
